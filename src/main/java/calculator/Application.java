1f14d2b8fde07f1cc5beb54f16a45012b0ae671e
donggeon <dkms625@naver.com>
2025-10-16 23:01:32 +0900

    feat(app): add application entry point

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,13 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+
+/**
+ * 계산기 프로그램을 실행하는 애플리케이션의 진입점입니다.
+ */
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        CalculatorController controller = new CalculatorController();
+        controller.run();
     }
 }
