a5cbf1cbda2d814444931a479b32ba9ea58bda7a
donggeon <dkms625@naver.com>
2025-10-17 23:32:14 +0900

    feat(controller): add run function

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -4,6 +4,8 @@ import calculator.model.Calculator;
 import calculator.view.InputView;
 import calculator.view.OutputView;
 
+import java.util.NoSuchElementException;
+
 /**
  * 계산기 프로그램의 흐름을 제어하는 컨트롤러 클래스입니다.
  */
@@ -21,8 +23,21 @@ public class CalculatorController {
 
     /**
      * 계산기 프로그램을 실행합니다.
+     * 사용자로부터 입력을 받고 결과를 출력합니다.
+     * @throws NoSuchElementException 콘솔 입력이 존재하지 않을 경우 발생
      */
     public void run() {
+        try {
+            String input = inputView.readInput();
 
+            // '\n' 문자열을 실제 개행 문자로 변환
+            input = input.replace("\\n", "\n");
+
+            int result = calculator.calculate(input);
+            outputView.printResult(result);
+
+        } catch (NoSuchElementException e) {
+            outputView.printResult(0);
+        }
     }
 }
